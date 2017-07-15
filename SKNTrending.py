b7887d3c4fbb67f01d499b907c5ac81eb556ed49
Amit Shah <amitmshah74@gmail.com>
2017-07-15 16:03:43 -0400

    This now posts all the symbols that are mentioned in trending articles. Still need to work on grouping them together to get counts. Will incorporate using panadas most likely.

--- SKNTrending.py
+++ SKNTrending.py
@@ -2,17 +2,45 @@
 trending stocks from Seeking Alpha -- it is ONLY for Non-Commercial
 Personal use'''
 from bs4 import BeautifulSoup
+# from BeautifulSoup import BeautifulStoneSoup as Soup
+
+from lxml import etree
+from lxml import html
+import requests
+import itertools
+import pandas
 
 # need to go to the XML trending analysis site for SKNAlpha
 # https://seekingalpha.com/listing/most-popular-articles.xml
 
-trending = BeautifulSoup('https://seekingalpha.com/listing/most-popular-articles.xml'
-, 'lxml')
+# put URL into URL
+url = 'https://seekingalpha.com/listing/most-popular-articles.xml'
 
-print("here is the link:")
-print (trending)
+#just to print onto terminal - can comment out /delete later
+print('URL via just print')
+print(url)
 
+# this puts the page url into the variable page
+page = requests.get(url)
+#create the tree element to hold the text of the xml file
+tree = page.text
+
+#parse the xml to get the links for the symbols
+soup = BeautifulSoup(tree,"xml")
+
+
+#printing out links, need to parse out just symbols
 print("looking for the various symbols:")
-for category in trending.find_all(True):
-    print(category.name)
-print("this worked.")
+symbols = []
+# i = 0
+
+for category in soup.findAll('category', {'type' : 'symbol'}):
+    symbols.append(category.string)
+    # i += 1
+
+symbols.groupby('symbols').count.nunique
+#count the number of symbols in trending articles
+
+
+
+print("program has finished")
